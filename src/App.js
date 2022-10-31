6aaf89468645e6aa2579b36b76285c8d2f5c35cb
paullaster <paullasterokoth98@gmail.com>
2022-10-31 16:15:00 +0300

    react files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [list, setList] = React.useState ([
+    {id: 1, name: 'picker', value: 4},
+    {id: 2, name: 'proof', value: 6},
+    {id: 3, name: 'applet', value: 9},
+    {id: 4, name: 'oranges', value: 3}
+  ])
+  const 
+  return (
+    <div className="App">
+      <ul>
+        {
+          list.map ( (item) => {
+            return (
+              <li>
+                <span>{item.name}</span> : <span>{item.value}</span>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
